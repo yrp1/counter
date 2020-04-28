b74df8982984e8703894981554a1e51e821fe4d1
yrp1 <61746292+yrp1@users.noreply.github.com>
2020-04-27 21:15:46 -0400

    replacing connect to new hooks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
 import React from "react"
-import {connect} from "react-redux"
+import {useSelector,useDispatch} from "react-redux"
 import {increment,decrement} from "./redux"
 
 
 function App(props){
+    const count = useSelector(state => state)
+    const dispatch = useDispatch()
     return(
         <div className = 'f1 tc'>
-            <h1 >{props.count}</h1>
+            <h1 >{count}</h1>
             <button 
                 className = 'b bg-light-green bn f3 ma4 br4 pa4' 
-                onClick = {props.decrement}> Decrease  
+                onClick = {()=>dispatch(decrement())}> Decrease  
             </button>
             <button 
                 className = 'b bg-light-green bn f3 ma4 br4 pa4' 
-                onClick = {props.increment}> Increase  
+                onClick = {()=>dispatch(increment())}> Increase  
             </button>
         </div>
     )
 }
 
- function mapStateToProps(globalState){
-    return{
-        count : globalState
-    }
-}
-
-const mapDispatchToProps = {
-    increment : increment,
-    decrement : decrement
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(App)
+export default App
 
